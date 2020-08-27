bdb4708047db4994f9722a84decb07eee4cff0f3
Jeremy Wood <js.wood206@gmail.com>
2020-08-27 07:40:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,7 @@ def key_for_min_value(name_hash)
   lowest_price = 1000000000
   name_hash.each do |item, price|
     if price < lowest_price
+      lowest_price = price
       cheapest_item = item
     end
   end
