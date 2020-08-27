e608895615ac5cc3c3713774aa961ede9f5c4787
Jeremy Wood <js.wood206@gmail.com>
2020-08-27 07:49:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if hash.length == 0
+  if name_hash.length == 0
     return
+    
   else
     cheapest_item = []
     lowest_price = 1000000000
